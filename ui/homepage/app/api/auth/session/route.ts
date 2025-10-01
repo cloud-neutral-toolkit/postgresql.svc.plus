f76766bc1b9ea69510a977a2002e849539d32165
shenlan <manbuzhe2009@qq.com>
2025-10-01 12:33:12 +0800

    fix: allow dynamic session api (#343)

--- ui/homepage/app/api/auth/session/route.ts
+++ ui/homepage/app/api/auth/session/route.ts
@@ -1,6 +1,8 @@
 import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 
+export const dynamic = 'force-dynamic'
+
 import { getAccountServiceBaseUrl } from '@lib/serviceConfig'
 
 const ACCOUNT_SERVICE_URL = getAccountServiceBaseUrl()
